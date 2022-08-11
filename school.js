7719dc6a1fe8ff53b2a94a26b0208652f2949d6f
Cyrillo46 <yahooisbetter46@gmail.com>
2022-08-11 18:24:42 -0400

    react practice

--- school.js
+++ school.js
@@ -1,1 +1,38 @@
+const Profile = () => {
+  return (
+    <div className="container">
+      <div className="top">
+        <h1>Profile</h1>
+        <img src="http://placehold.it/" />
+      </div>
 
+      <div className="profile-info">
+        <img src="http://placehold.it/" />
+        <p>Name</p>
+        <a>
+          <img src="http://placehold.it" />
+        </a>
+        <a>
+          <img src="http://placehold.it" />
+        </a>
+      </div>
+
+      <div className="awards">
+        <h3>Awards</h3>
+        <img src="http://placehold.it/" />
+        <img src="http://placehold.it/" />
+        <img src="http://placehold.it/" />
+        <img src="http://placehold.it/" />
+      </div>
+
+      <div className="friends-leaderboard">
+        <h3>Friends Leaderboard</h3>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
+    </div>
+  );
+};
